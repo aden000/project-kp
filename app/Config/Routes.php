12fe909f774d225686718272c81fa51c050259c0
Ade <ade.nugroho123@gmail.com>
2020-10-13 18:12:47 +0700

    Adding some Maintenance page
    - Menambah konfigurasi baru pada env 'isMaintenance' tipe boolean (true atau false)
    - Menambah Controller yang mengarahkan ke view MaintenancePage

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,43 +31,48 @@ $routes->setAutoRoute(false);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'DefaultController::index', ['as' => 'home']);
-$routes->get('about', 'DefaultController::about', ['as' => 'about']);
-$routes->get('artikel/(:segment)', 'DefaultController::artikel/$1', ['as' => 'detail.artikel']);
-$routes->post('artikel', "DefaultController::handleCommentCreate", ['as' => 'handle.comment']);
-//$routes->get('debug/(:segment)', "AdminController::getEncrypted/$1");
+if (!env('isMaintenance')) {
+	$routes->get('/', 'DefaultController::index', ['as' => 'home']);
+	$routes->get('about', 'DefaultController::about', ['as' => 'about']);
+	$routes->get('artikel/(:segment)', 'DefaultController::artikel/$1', ['as' => 'detail.artikel']);
+	$routes->post('artikel', "DefaultController::handleCommentCreate", ['as' => 'handle.comment']);
+	//$routes->get('debug/(:segment)', "AdminController::getEncrypted/$1");
 
-$routes->group('admin', function ($routes) {
-	$routes->post('login', 'AdminController::loginProcess', ['as' => 'admin.login.process']);
-	$routes->post('logout', 'AdminController::logoutProcess', ['as' => 'admin.logout.process']);
-	$routes->group('artikel', function ($routes) {
-		$routes->get('/', 'AdminController::manageArticle', ['as' => 'admin.artikel']);
-		$routes->get('create', 'AdminController::articleCreate', ['as' => 'admin.artikel.create']);
-		$routes->post('create', 'AdminController::articleCreateProcess', ['as' => 'admin.artikel.create.process']);
-		$routes->get('edit/(:num)', 'AdminController::articleEdit/$1', ['as' => 'admin.artikel.edit']);
-		$routes->post('edit/(:num)', 'AdminController::articleEditProcess/$1', ['as' => 'admin.artikel.edit.process']);
-		$routes->post('delete/(:num)', 'AdminController::articleDeleteProcess/$1', ['as' => 'admin.artikel.delete.process']);
-		if (env('enableComment')) {
-			$routes->group('comment', function ($routes) {
-				$routes->get('(:num)', 'CommentController::index/$1', ['as' => 'admin.artikel.comment']);
-				$routes->post('ajax', 'CommentController::kelolaAJAX');
-				$routes->post('edit', 'CommentController::editComment', ['as' => 'admin.artikel.comment.edit']);
-			});
-		}
+	$routes->group('admin', function ($routes) {
+		$routes->post('login', 'AdminController::loginProcess', ['as' => 'admin.login.process']);
+		$routes->post('logout', 'AdminController::logoutProcess', ['as' => 'admin.logout.process']);
+		$routes->group('artikel', function ($routes) {
+			$routes->get('/', 'AdminController::manageArticle', ['as' => 'admin.artikel']);
+			$routes->get('create', 'AdminController::articleCreate', ['as' => 'admin.artikel.create']);
+			$routes->post('create', 'AdminController::articleCreateProcess', ['as' => 'admin.artikel.create.process']);
+			$routes->get('edit/(:num)', 'AdminController::articleEdit/$1', ['as' => 'admin.artikel.edit']);
+			$routes->post('edit/(:num)', 'AdminController::articleEditProcess/$1', ['as' => 'admin.artikel.edit.process']);
+			$routes->post('delete/(:num)', 'AdminController::articleDeleteProcess/$1', ['as' => 'admin.artikel.delete.process']);
+			if (env('enableComment')) {
+				$routes->group('comment', function ($routes) {
+					$routes->get('(:num)', 'CommentController::index/$1', ['as' => 'admin.artikel.comment']);
+					$routes->post('ajax', 'CommentController::kelolaAJAX');
+					$routes->post('edit', 'CommentController::editComment', ['as' => 'admin.artikel.comment.edit']);
+				});
+			}
+		});
+		$routes->group('kategori', function ($routes) {
+			$routes->get('/', 'AdminController::manageKategori', ['as' => 'admin.kategori']);
+			$routes->post('ajax', 'AdminController::kelolaAJAX');
+			$routes->post('create', 'AdminController::kategoriCreateProcess', ['as' => 'admin.kategori.create.process']);
+			$routes->post('edit', 'AdminController::kategoriEditProcess', ['as' => 'admin.kategori.edit.process']);
+			$routes->post('delete', 'AdminController::kategoriDeleteProcess', ['as' => 'admin.kategori.delete.process']);
+		});
+		$routes->group('user', function ($routes) {
+			$routes->post('changepass', 'UserController::changePassword', ['as' => 'admin.user.changepass']);
+			$routes->get('/', 'UserController::manageUser', ['as' => 'admin.user.manage']);
+			$routes->post('create', 'UserController::createUser', ['as' => 'admin.user.create']);
+		});
 	});
-	$routes->group('kategori', function ($routes) {
-		$routes->get('/', 'AdminController::manageKategori', ['as' => 'admin.kategori']);
-		$routes->post('ajax', 'AdminController::kelolaAJAX');
-		$routes->post('create', 'AdminController::kategoriCreateProcess', ['as' => 'admin.kategori.create.process']);
-		$routes->post('edit', 'AdminController::kategoriEditProcess', ['as' => 'admin.kategori.edit.process']);
-		$routes->post('delete', 'AdminController::kategoriDeleteProcess', ['as' => 'admin.kategori.delete.process']);
-	});
-	$routes->group('user', function ($routes) {
-		$routes->post('changepass', 'UserController::changePassword', ['as' => 'admin.user.changepass']);
-		$routes->get('/', 'UserController::manageUser', ['as' => 'admin.user.manage']);
-		$routes->post('create', 'UserController::createUser', ['as' => 'admin.user.create']);
-	});
-});
+} else {
+	$routes->get('/', 'MaintenanceController::index');
+}
+
 
 /**
  * --------------------------------------------------------------------
---
+++ app/Controllers/MaintenanceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class MaintenanceController extends BaseController
+{
+    public function index()
+    {
+        return view('MaintenancePage');
+    }
+}
