8dae2a18246c170ef17ee8fa73d3dba88dbb4e98
AnEmotionless <yusufuus10@gmail.com>
2020-12-06 16:38:05 +0700

    Update Dashboard.php

--- app/Views/Admin/Dashboard.php
+++ app/Views/Admin/Dashboard.php
@@ -3,10 +3,16 @@
 <?php if (session('message')) : ?>
     <div class="flash-data" data-judul="<?= session('message')['judul']; ?>" data-msg="<?= session('message')['msg']; ?>" data-role="<?= session('message')['role']; ?>"></div>
 <?php endif; ?>
+<?php
+
+use App\Models\UserModel; ?>
+<?php $a = new UserModel();
+$a = $a->find(session()->get('whoLoggedIn'));  ?>
 <div class="row mx-3 my-4">
     <div class="col shadow-sm border border-dark pt-2 pb-2">
         <h4 class="mb-3"><i class="fa fa-user"></i> Selamat datang, <?= $user['nama_user']; ?></h4>
         <div class="row">
+            <?php if ($a['role'] == 0 || $a['role'] == 1) : ?>
             <div class="col-md-3 mb-3">
                 <div class="card text-white bg-primary">
                     <div class="card-body">
@@ -31,21 +37,23 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 mb-3">
-                <div class="card text-white bg-success">
-                    <div class="card-body">
-                        <h3 class="card-title"><?= count($allGaleri); ?></h3>
-                        <p class="card-text">Gambar pada slider galeri</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row mt-md-2">
+            <?php endif; ?>
+            <?php if ($a['role'] == 0 || $a['role'] == 2) : ?>
             <div class="col-md-3 mb-3">
                 <div class="card text-white bg-success">
                     <div class="card-body">
                         <h3 class="card-title"><?= count($allDokumen); ?></h3>
                         <p class="card-text">Total dokumen</p>
+                    </div>
+                </div>
+            </div>
+            <?php endif; ?>
+            <?php if ($a['role'] == 0 ) : ?>
+            <div class="col-md-3 mb-3">
+                <div class="card text-white bg-success">
+                    <div class="card-body">
+                        <h3 class="card-title"><?= count($allGaleri); ?></h3>
+                        <p class="card-text">Gambar pada slider galeri</p>
                     </div>
                 </div>
             </div>
@@ -73,8 +81,6 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row mt-md-2">
             <div class="col-md-3 mb-3">
                 <div class="card text-white bg-success">
                     <div class="card-body">
@@ -83,6 +89,7 @@
                     </div>
                 </div>
             </div>
+            <?php endif; ?>
         </div>
     </div>
 </div>
